7debee3bc6eac60f6595817e75c1bded1401fde9
Anusha Zia <anushazia@Ammars-MBP.fios-router.home>
2021-06-13 22:51:37 -0400

    Form is working

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
     <>
       <nav className="navbar">
         <button><Link to="/">Home</Link></button>
-        <button><Link to="/pizza">Pizzas</Link></button>
+        <button><Link to="/pizza">Order Pizza</Link></button>
       </nav>
       <h1>Lambda Eats</h1>
       <img src={Pizza} alt= "pizza"></img>
